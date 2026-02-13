eb50d7de8da13653a36d0efbd0f4ce4afbde9992
AlexdanTB <alexdanieltb220@gmail.com>
2026-02-13 10:01:33 -0500

    feat: añade a detalleprod-page a las rutas app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { DetalleProdPage } from './features/detalle-prod-page/detalle-prod-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'producto', component: DetalleProdPage}
+];
