0fb6e75da78eb001f7314ee2b48e8cf625354cea
AlexdanTB <alexdanieltb220@gmail.com>
2026-02-13 00:28:26 -0500

    feat: define el arreglo de enlaces para footer

--- src/app/shared/footer/footer.ts
+++ src/app/shared/footer/footer.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Enlace } from '../../models/enlace';
 
 @Component({
   selector: 'app-footer',
@@ -7,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrl: './footer.css',
 })
 export class Footer {
+
+  enlaces: Enlace[] = [
+    {titulo:"Inicio", link:""},
+    {titulo:"Catálogo", link:""},
+    {titulo:"Nosotros", link:""},
+    {titulo:"Contacto", link:""},
+  ]
 
 }
