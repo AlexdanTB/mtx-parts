170675c8bc1912f8d83bcab5319c5341ad91feac
AlexdanTB <alexdanieltb220@gmail.com>
2026-02-12 23:04:30 -0500

    feat: creación de componente productos-grid

---
+++ src/app/shared/productos-grid/productos-grid.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-productos-grid',
+  imports: [],
+  templateUrl: './productos-grid.html',
+  styleUrl: './productos-grid.css',
+})
+export class ProductosGrid {
+
+}
