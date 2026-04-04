ed3d253f3736cb2b4bc8cd75b396f68d590ac4ba
andergh-0405 <guabilanderson33@gmail.com>
2026-04-03 20:31:59 -0500

    Creacion de la pagina de contacto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomePage } from './features/home-page/home-page';
+import { ContactoPage } from './features/contacto-page/contacto-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{path:'',component:HomePage},
+{path:'contacto',component:ContactoPage}
+
+];
