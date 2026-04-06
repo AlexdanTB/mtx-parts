8d128d0cc75816ad4d4b6b0076325936cd249758
andergh-0405 <guabilanderson33@gmail.com>
2026-04-06 16:17:40 -0500

    Correcion de guardian

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { ProductosPage } from './features/productos-page/productos-page';
 import { Register } from './shared/register/register';
 import { Login } from './shared/login/login';
 import { canActiveGuard } from './guards/can-active-guard';
+import { LoginRegisterPage } from './features/login-register-page/login-register-page';
 
 export const routes: Routes = [
     { path: '', component: HomePage },
@@ -13,7 +14,8 @@ export const routes: Routes = [
     {path: 'nosotros', component: NosotrosPage },
     { path: 'repuestos', component: ProductosPage },
     {path:'registrarse',component: Register},
-    {path:'login',component: Login,canActivate: [canActiveGuard]},
+    {path:'logins',component:LoginRegisterPage ,canActivate: [canActiveGuard]},
+    {path:'login',component:Login ,}
 
 
 ];
--- src/app/guards/can-active-guard.ts
+++ src/app/guards/can-active-guard.ts
@@ -3,12 +3,12 @@ import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../service/auth-service';
 
 export const canActiveGuard: CanActivateFn = (route, state) => {
-    const servicioAuth = inject(AuthService);
+
+  const servicioAuth = inject(AuthService);
   const router = inject(Router);
 
-  if(servicioAuth.sesionIniciada()){
+  if (servicioAuth.sesionIniciada()) {
     return true;
   }
-
   return router.parseUrl('/login');
 };
