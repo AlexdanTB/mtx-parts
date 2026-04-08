5fa57b0b0766394c1e9b8e712c3ba89999f377d7
AlexdanTB <alexdanieltb220@gmail.com>
2026-04-07 21:39:32 -0500

    config: firebase api key

---
+++ src/app/config/firebase.config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyALFKSpgY5_NuuUJwRwyt4UuKuAXK8qmcQ",
+  authDomain: "mtx-awii.firebaseapp.com",
+  projectId: "mtx-awii",
+  storageBucket: "mtx-awii.firebasestorage.app",
+  messagingSenderId: "2305392802",
+  appId: "1:2305392802:web:4ffd5034705f46fce597fb"
+};
