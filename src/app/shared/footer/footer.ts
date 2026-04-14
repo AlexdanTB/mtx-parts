1023137a54945db4f11046b0c31e94ea1334c2ef
andergh-0405 <guabilanderson33@gmail.com>
2026-04-13 22:14:49 -0500

    Ajustes de diseño

--- src/app/shared/footer/footer.ts
+++ src/app/shared/footer/footer.ts
@@ -1,19 +1,20 @@
 import { Component } from '@angular/core';
 import { Enlace } from '../../models/enlace';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './footer.html',
   styleUrl: './footer.css',
 })
 export class Footer {
 
   enlaces: Enlace[] = [
-    {titulo:"Inicio", link:""},
-    {titulo:"Catálogo", link:""},
-    {titulo:"Nosotros", link:""},
-    {titulo:"Contacto", link:""},
+    {titulo:"Inicio", link:"/"},
+    {titulo:"Catálogo", link:"/repuestos"},
+    {titulo:"Nosotros", link:"/nosotros"},
+    {titulo:"Contacto", link:"/contacto"},
   ]
 
 }
